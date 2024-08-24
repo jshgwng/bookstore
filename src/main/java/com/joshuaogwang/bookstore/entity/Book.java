f99a7d1b933b778beaa115de1c125b6689e2f6f6
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-08-24 10:08:35 +0300

    initialize book entity fields

--- src/main/java/com/joshuaogwang/bookstore/entity/Book.java
+++ src/main/java/com/joshuaogwang/bookstore/entity/Book.java
@@ -3,9 +3,16 @@ package com.joshuaogwang.bookstore.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "books")
 @Entity
 public class Book {
     private Long id;
